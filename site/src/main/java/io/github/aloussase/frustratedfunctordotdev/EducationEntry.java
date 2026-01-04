9b4e8b030a04db75334155703f59324cefe1cca9
Alexander Goussas <alexandergoussas.thoughtworks@latam.com>
2026-01-04 00:04:04 -0500

    update cv section

--- site/src/main/java/io/github/aloussase/frustratedfunctordotdev/EducationEntry.java
+++ site/src/main/java/io/github/aloussase/frustratedfunctordotdev/EducationEntry.java
@@ -11,10 +11,16 @@ public record EducationEntry(
     public static List<EducationEntry> entries() {
         return List.of(
                 new EducationEntry(
+                        "Chez moi",
+                        "4ever",
+                        "Dark Master of Electronic Arts",
+                        "I am always reading something or watching someone else read something. Life's too short not to fill every waking and sleeping hour with knowledge."
+                ),
+                new EducationEntry(
                         "Escuela Superior Politécnica del Litoral",
                         "2019 - Abandoned",
                         "Bsc. Computer Science",
-                        ""
+                        "Started with Biology, switched to CS on my 3rd semester. Can't say I learned anything here honestly. I'm sure everyone had good intentions, but intentions never really count towards anything."
                 )
         );
     }
--- site/src/main/java/io/github/aloussase/frustratedfunctordotdev/WorkEntry.java
+++ site/src/main/java/io/github/aloussase/frustratedfunctordotdev/WorkEntry.java
@@ -15,27 +15,28 @@ public record WorkEntry(
                         "Thoughtworks",
                         "December 2024 - Present",
                         "Consultant",
-                        "Worked on the migration of the shopping module of a loyalty platform from a webview to a native mobile experience with SpringBoot and ReactNative."
+                        "Love this place. Been learning a lot and doing lots of fun stuff. Also, had my first service outage and that was ... not so fun, but quite an enriching experience! \uD83D\uDCAF would recommend"
                 ),
                 new WorkEntry(
                         "Tia S.A.",
                         "June 2024 - December 2024",
                         "Software Developer",
-                        "Created a transactional API with SpringBoot for one of the company's partners to be able to give credit to their employees." +
-                                "Also dockerized some ETL scripts in Python and designed database schemas for a new internal application that never came into existence."
+                        "The only interesting thing I did here was building a REST API in SpringBoot for an external partner to give credit points to their employees. Even so, most of the logic was in the database (not my choice) and my service only invoked the stored procedures in it. This took me about 2 weeks and the rest of the time I did literally nothing work-related. And my experience is that national industry is like this: managers like to complain but they don't know how to manage teams nor resources, and almost no one with a tech product actually knows how to produce good tech."
                 ),
                 new WorkEntry(
                         "Escuela Superior Politécnica del Litoral",
                         "June 2024 - December 2024",
                         "Software Developer",
-                        "Created an application to monitor students' mental health using .NET and SvelteKit. Here I also managed and automated deployments to our server and " +
-                                "designed the application's database schema."
+                        """
+                                A mobile app with Flutter and accompanying API with .NET. Also, managed deployments on a provided server and automated them by installing a GitLab runner on it. Was a pretty fun experience since I had complete control over the stack.
+                                
+                                Fun story: I (along with a colleague) rewrote the entire first version of the web dashboard from Next.js to SvelteKit. Of course, I asked for permission first and was granted it. Then, when the time came to hand over the app, everyone lost their minds because IT SHOULD BE WRITTEN IN REACT!!! WE ARE REACT DEVELOPERS WE CAN'T MAINTAIN A SVELTEKIT APP!!!! JAVASCRIPT? NEVER HEARD OF 'ER."""
                 ),
                 new WorkEntry(
                         "Industrial Pesquera Santa Priscila",
                         "Februray 2024 - May 2024",
                         "Internship",
-                        "Built web forms for the quality control module of the company's ERP using Laravel and Vue.js"
+                        "Boring forms with Vue and Laravel"
                 )
         );
     }
